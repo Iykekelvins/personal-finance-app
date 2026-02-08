f8581911565a745e4edcd4eeebcf0a77925c3886
Kelvin Ochubili <kelvinochubili@gmail.com>
2026-02-08 15:05:00 +0100

    feat: install clerk for user authentication

---
+++ proxy.ts
@@ -0,0 +1,27 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+const isPublicRoute = createRouteMatcher([
+	'/sign-in(.*)',
+	'/sign-up(.*)',
+	'/forgot-password(.*)',
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+	const { userId } = await auth();
+
+	if (userId && isPublicRoute(req)) {
+		return NextResponse.redirect(new URL('/', req.url));
+	}
+
+	if (!isPublicRoute(req)) {
+		await auth.protect();
+	}
+});
+
+export const config = {
+	matcher: [
+		'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+		'/(api|trpc)(.*)',
+	],
+};
